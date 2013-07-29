9329fe64c87b005f1a4deccbb3135ed76af046e1
Nick Muerdter <nick.muerdter@nrel.gov>
2013-07-29 16:56:45 -0600

    Use separate acl cookbook.

--- definitions/acl.rb
+++
@@ -1,6 +0,0 @@
-define :acl do
-  acl = "group:#{params[:group]}:#{params[:modify]}"
-  execute "setfacl -m #{acl} #{params[:name]}" do
-    not_if "getfacl #{params[:name]} | grep -x '^#{params[:modify]}$'"
-  end
-end
