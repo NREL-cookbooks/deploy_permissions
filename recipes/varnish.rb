4683a7dec8d2c933ed66bec6ca8f5984c2662382
Nick Muerdter <nick.muerdter@nrel.gov>
2013-10-07 14:40:51 -0600

    Add sudo deploy permissions for varnish.

---
+++ recipes/varnish.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: deploy_permissions
+# Recipe:: varnish
+#
+# Copyright 2013, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "sudo"
+
+sudo "deploy_permissions_varnish" do
+  group node[:deploy_permissions][:group_name]
+
+  commands [
+    "/usr/local/bin/varnish_ban",
+  ]
+
+  host "ALL"
+  nopasswd true
+end
