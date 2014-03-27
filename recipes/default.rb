689b3171dfaafcd1922f43ada7312dfb9ca808f5
Nick Muerdter <nick.muerdter@nrel.gov>
2014-03-27 14:33:49 -0600

    Use the setgid and default ACLs to make group permissions more sane.
    Now anything going on inside the deployment directory should always
    continue to be group writable by the deploy group. This should hopefully
    reduce the need for things to be chmod-ed or chgrp-ed after the fact.

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,6 +20,23 @@ node[:deploy_permissions][:writable_dirs].each do |dir|
   directory(dir) do
     user "root"
     group node[:deploy_permissions][:group_name]
-    mode "775"
+
+    # Add the setgid bit so all child files/directories automatically become
+    # owned by this same deploy group.
+    mode "2775"
+  end
+
+  # Set a default ACL on the directory allowing the deploy group full
+  # permissions. This default ACL will automatically be applied to all children
+  # items.
+  #
+  # This combined with the setgid allow should ensure that the group maintains
+  # full access to everything in this directory. Based on:
+  # http://brunogirin.blogspot.ca/2010/03/shared-folders-in-ubuntu-with-setgid.html
+  acl(dir) do
+    default true
+    recursive true
+    group node[:deploy_permissions][:group_name]
+    modify "rwx"
   end
 end
